206d64d5cfe84bbae769fa78128ee0d3b65089f2
lazywizmc <83636490+lazywizmc@users.noreply.github.com>
2023-11-05 02:08:29 +0000

    RePurpose - Serving the People for the People

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const wrapper = document.querySelector('.wrapper');
 
-const  btnpopup = document.querySelector('.btnlogin-popup');
+const btnpopup = document.querySelector('.btnlogin-popup');
 const iconclose = document.querySelector('.icon-close');
 
-btnpopup.addEventListener('click', () => {wrapper.classList.add('active-popup');});
+btnpopup.addEventListener('click', () => { wrapper.classList.add('active-popup'); });
 /*const  btnpopup = document.querySelector('.btn-popup');
 
 btnpopup.addEventListener('click', () => {wrapper.classList.add('active-popup');});*/
-iconclose.addEventListener('click', () => {wrapper.classList.remove('active-popup');});
-                                         
+iconclose.addEventListener('click', () => { wrapper.classList.remove('active-popup'); });
 
+document.addEventListener("scroll", function() {
+  let textElement = document.getElementById('moving-text-box');
+  if (window.scrollY > 0) {
+    textElement.parentElement.classList.add('scrolling-down');
+  } else {
+    textElement.parentElement.classList.remove('scrolling-down');
+  }
+});
